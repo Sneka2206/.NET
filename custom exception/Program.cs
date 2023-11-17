ae3947103e91f2ad17bdab5409213fb9304421c4
Sneka S <114380237+Sneka2206@users.noreply.github.com>
2023-11-17 10:34:01 +0530

    Add files via upload

---
+++ custom exception/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+public class AgeException : Exception
+{
+    public AgeException(String message)
+        : base(message)
+    {
+
+    }
+}
+public class TestUserDefinedException
+{
+    static void validate(int age)
+    {
+        if (age < 18)
+        {
+            throw new AgeException("Sorry, Age must be greater than 18");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        try
+        {
+            validate(13);
+        }
+        catch (AgeException e)
+        {
+            Console.WriteLine(e);
+        }
+
+        Console.WriteLine("Rest of the code");
+        Console.ReadLine();
+
+    }
+}
+
